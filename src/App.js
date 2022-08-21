11bf5b784039d99ab399f3cbfc2fcfbf40659a17
Russell Fraze <russell.fraze@gmail.com>
2022-08-21 13:45:23 -0700

    useCallback example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import UseMemoExample from './components/UseMemoExample'
+import UseCallbackExample from './components/UseCallbackExample'
 
 function App() {
 
   return (
     <div className="App">
-      <UseMemoExample />
+      <UseCallbackExample />
     </div>
   );
 }
